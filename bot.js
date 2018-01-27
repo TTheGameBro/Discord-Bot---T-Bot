913b047ef704c18ce3e656436a15ecf1e7df95bb
TTheGameBro <35857154+TTheGameBro@users.noreply.github.com>
2018-01-26 21:06:22 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+const token = "Mzk3MjU4ODUxNjg0MDU3MDkz.DStXpw.axfxD8ryyfAJxP_Hgs1rCS25FBs";
+const prefix = "-";
+
+//Command Log message
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    console.log("Ready");
+});    
+
+client.login(procees.env.BOT_TOKEN);
